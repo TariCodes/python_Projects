e9754e35e252a4bbbfc44183b099f4b2f900d7ac
Godbless Ikadoi <godblessika@gmail.com>
2025-04-28 17:16:53 +0100

    created number guessing game

---
+++ number_guessing_game.py
@@ -0,0 +1,32 @@
+# Author: Godbless Ikadoi
+# TItle: Number Guessing Game
+# Description: A fun and intriguing game which lets the user to guess a random number, the game starts and if the user gets it right He/She gets a congrats message, else it prompts the user to make more tries till He/She gets the right number 
+# Date: 28-04-25
+
+import random
+
+print("Hi welcome to the game, This is a number guessing game.\nYou got 7 chances to guess the number. Let's start the game")
+
+number_to_guess = random.randrange(100)
+
+chances = 7
+
+guess_counter = 0
+
+while guess_counter < chances:
+
+    guess_counter += 1
+    my_guess = int(input('Please Enter your Guess : '))
+
+    if my_guess == number_to_guess:
+        print(f'The number is {number_to_guess} and you found it right !! in the {guess_counter} attempt')
+        break
+
+    elif guess_counter >= chances and my_guess != number_to_guess:
+        print(f'Oops sorry, The number is {number_to_guess} better luck next time')
+
+    elif my_guess > number_to_guess:
+        print('Your guess is higher ')
+
+    elif my_guess < number_to_guess:
+        print('Your guess is lesser')
